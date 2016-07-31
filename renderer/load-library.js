c9c3e0f2393533e52339c00c61cdd4ff1eb51e74
Nate Goldman <ungoldman@gmail.com>
2016-07-30 18:00:18 -0700

    remove unused modules, include mp3s

--- renderer/load-library.js
+++ renderer/load-library.js
@@ -1,14 +1,12 @@
 const fs = require('fs')
 const waterfall = require('run-waterfall')
-const parallelLimit = require('run-parallel-limit')
 const path = require('path')
-const pump = require('pump')
 
 const mediaLib = path.resolve(__dirname, '../mock-lib')
 
 waterfall([
   getFiles,
-  filterForMp3s,
+  filterForAudio,
   log], function (err) {
   if (err) throw (err)
 })
@@ -17,13 +15,14 @@ function getFiles (cb) {
   fs.readdir(mediaLib, cb)
 }
 
-function filterForMp3s (files, cb) {
+function filterForAudio (files, cb) {
   var mp3Files = files.filter(checkFilename)
   cb(null, mp3Files)
 }
 
 function checkFilename (file) {
-  return file.includes('.m4a')
+  var extensions = ['.m4a', '.mp3']
+  return extensions.find(ext => file.includes(ext))
 }
 
 function log (mp3Files, cb) {
