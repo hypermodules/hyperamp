a3ac47beaf39e75208a5a5c3004e0d13c7f6abd6
Bret Comnes <bcomnes@gmail.com>
2016-08-07 17:40:12 -0700

    Use modularized router fix

--- renderer/index.js
+++ renderer/index.js
@@ -2,11 +2,11 @@ const choo = require('choo')
 const view = require('./views/main')
 const preferences = require('./views/preferences')
 const model = require('./models/main')
-const location = require('./models/location')
+const location = require('choo-location-electron')
 const app = choo()
 
 app.model(model)
-app.model(location())
+app.model(location)
 app.router(route => [
   route('/', view),
   route('/preferences', preferences)
