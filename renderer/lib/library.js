000269f8307a15a8d3fe142383ffdc9638d5b729
Nate Goldman <ungoldman@gmail.com>
2016-08-09 10:26:26 -0700

    refactor stream lib a tiny bit

--- renderer/lib/library.js
+++ renderer/lib/library.js
@@ -2,31 +2,33 @@ const fs = require('fs')
 const path = require('path')
 const walker = require('folder-walker')
 const mm = require('musicmetadata')
-
-const extensions = ['.m4a', '.mp3']
+const validExtensions = ['.m4a', '.mp3']
 
 module.exports = (libPath, cb) => {
   walker([libPath]).on('data', function (data) {
     if (!isValidFile(data)) return
-
-    mm(fs.createReadStream(data.filepath), function (err, metadata) {
-      if (err) return cb(err)
-
-      // append path to metadata
-      metadata.path = data.filepath
-
-      // default title is file name
-      if (metadata.title === '') {
-        let { basename } = data
-        metadata.title = path.basename(basename, path.extname(basename))
-      }
-
-      cb(null, metadata)
-    })
+    parseMetadata(data, cb)
   })
 }
 
 function isValidFile (data) {
   if (data.type !== 'file') return false
-  return extensions.includes(path.extname(data.basename))
+  return validExtensions.includes(path.extname(data.basename))
+}
+
+function parseMetadata (data, cb) {
+  mm(fs.createReadStream(data.filepath), function (err, metadata) {
+    if (err) return cb(err)
+
+    // append path to metadata
+    metadata.path = data.filepath
+
+    // default title is file name
+    if (metadata.title === '') {
+      let { basename } = data
+      metadata.title = path.basename(basename, path.extname(basename))
+    }
+
+    cb(null, metadata)
+  })
 }
