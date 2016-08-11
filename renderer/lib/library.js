13ab45ecc1323a62de2d6774816570b1c365d58b
Nate Goldman <ungoldman@gmail.com>
2016-08-11 15:53:29 -0700

    limit metadata extraction
    fixes album art buffer issue

--- renderer/lib/library.js
+++ renderer/lib/library.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const path = require('path')
 const walker = require('folder-walker')
 const mm = require('musicmetadata')
-const validExtensions = ['.m4a', '.mp3']
+const validExtensions = ['m4a', 'mp3']
 
 module.exports = (libPath, cb) => {
-  walker([libPath]).on('data', function (data) {
+  walker([libPath]).on('data', data => {
     if (!isValidFile(data)) return
     parseMetadata(data, cb)
   })
@@ -13,22 +13,27 @@ module.exports = (libPath, cb) => {
 
 function isValidFile (data) {
   if (data.type !== 'file') return false
-  return validExtensions.includes(path.extname(data.basename))
+  let ext = path.extname(data.basename).substring(1)
+  return validExtensions.includes(ext)
 }
 
 function parseMetadata (data, cb) {
-  mm(fs.createReadStream(data.filepath), function (err, metadata) {
-    if (err) return cb(err)
+  let { filepath } = data
 
-    // append path to metadata
-    metadata.path = data.filepath
-
-    // default title is file name
-    if (metadata.title === '') {
-      let { basename } = data
-      metadata.title = path.basename(basename, path.extname(basename))
+  mm(fs.createReadStream(filepath), (err, meta) => {
+    if (err) {
+      err.message += ` (file: ${filepath})`
+      return cb(err)
     }
 
-    cb(null, metadata)
+    let { title, artist, album } = meta
+
+    if (!title) {
+      let { basename } = data
+      let ext = path.extname(basename)
+      title = path.basename(basename, ext)
+    }
+
+    cb(null, { title, artist, album, filepath })
   })
 }
