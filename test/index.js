89154de3e5da8314f49c81690b815511f6d65d8c
Bret Comnes <bcomnes@gmail.com>
2017-02-16 17:57:01 -0800

    Spectron tests (#86)
    * set up some shitty tests
    
    promises are the worst
    
    * tweak travis settings
    
    * fix travis script
    
    * window count is 4 in linux for some reason
    
    * more tweaks

---
+++ test/index.js
@@ -0,0 +1,34 @@
+var Application = require('spectron').Application
+var test = require('tape')
+var path = require('path')
+
+test('does the thing turn on?', t => {
+  var app = new Application({
+    path: getElectronPath(),
+    args: [path.join(__dirname, '..', 'main', 'index.js')]
+  })
+
+  app.start()
+  .then(() => {
+    t.pass('the app started up')
+    return app.client.waitUntilWindowLoaded().getWindowCount()
+  })
+  .then(count => {
+    t.is(count, 4, 'there are 4 browser windows for some reason')
+  })
+  .catch(err => {
+    t.error(err, 'an error occured. uh oh')
+  })
+  .then(() => {
+    app.stop()
+  })
+  .then(() => {
+    t.end()
+  })
+})
+
+function getElectronPath () {
+  var electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron')
+  if (process.platform === 'win32') electronPath += '.cmd'
+  return electronPath
+}
