7f192e80a89e0bf7178f1e79200af0d266498237
Bret Comnes <bcomnes@gmail.com>
2017-03-04 13:54:29 -0800

    move file loading to toolbar (for now)

--- renderer/views/components/toolbar.js
+++ renderer/views/components/toolbar.js
@@ -3,6 +3,7 @@ var css = require('csjs-inject')
 var player = require('./player')
 var search = require('./search')
 var button = require('./button')
+var { app, dialog } = require('electron').remote
 
 var style = css`
   .toolbar {
@@ -27,10 +28,29 @@ module.exports = (state, prev, send) => html`
     ${player(state.player, send)}
     <div class="${style.rightCluster}">
       ${search({ oninput: (e) => send('library:search', e.target.value) })}
-      ${button({
-        onclick: () => console.log('load songs here'),
-        iconName: 'entypo-plus'
-      })}
+      ${addFiles(send)}
     </div>
   </header>
 `
+
+function addFiles (send) {
+  function showDialog () {
+    dialog.showOpenDialog({
+      defaultPath: app.getPath('home'),
+      properties: ['openDirectory']
+    }, (paths) => {
+      // paths is undefined if user presses cancel
+      if (paths) {
+        send('config:set', { music: paths[0] })
+        send('library:loadSongs')
+      }
+    })
+  }
+
+  return html`
+    ${button({
+      onclick: showDialog,
+      iconName: 'entypo-plus'
+    })}
+  `
+}
