7891fe77eb55f6c075d60677e95e82bc3c87ce64
Bret Comnes <bcomnes@gmail.com>
2017-03-05 19:16:31 -0800

    use electron-default-menu

--- main/menu.js
+++ main/menu.js
@@ -1,4 +1,5 @@
 var { app, shell, Menu } = require('electron')
+var defaultMenu = require('electron-default-menu')
 var audio = require('./audio')
 var pkg = require('../package.json')
 
@@ -6,187 +7,37 @@ module.exports = {
   init
 }
 
-function init () {
-  var menu = Menu.buildFromTemplate(getTemplate())
-  Menu.setApplicationMenu(menu)
-}
+var menuTemplate = defaultMenu(app, shell)
+var viewMenu = menuTemplate[2].submenu
+var helpMenu = menuTemplate[4].submenu
 
-function getTemplate () {
-  var template = [
-    {
-      label: 'Edit',
-      submenu: [
-        {
-          label: 'Undo',
-          accelerator: 'CmdOrCtrl+Z',
-          role: 'undo'
-        },
-        {
-          label: 'Redo',
-          accelerator: 'Shift+CmdOrCtrl+Z',
-          role: 'redo'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Cut',
-          accelerator: 'CmdOrCtrl+X',
-          role: 'cut'
-        },
-        {
-          label: 'Copy',
-          accelerator: 'CmdOrCtrl+C',
-          role: 'copy'
-        },
-        {
-          label: 'Paste',
-          accelerator: 'CmdOrCtrl+V',
-          role: 'paste'
-        },
-        {
-          label: 'Select All',
-          accelerator: 'CmdOrCtrl+A',
-          role: 'selectall'
-        }
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
-          click (item, focusedWindow) {
-            if (focusedWindow) focusedWindow.webContents.reload()
-          }
-        },
-        {
-          label: 'Hard Reload (Clear Cache)',
-          accelerator: 'CmdOrCtrl+Shift+R',
-          click (item, focusedWindow) {
-            if (focusedWindow) focusedWindow.webContents.reloadIgnoringCache()
-          }
-        },
-        {
-          label: 'Toggle Full Screen',
-          accelerator: (function () {
-            if (process.platform === 'darwin') return 'Ctrl+Command+F'
-            return 'F11'
-          })(),
-          click (item, focusedWindow) {
-            if (focusedWindow) focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
-          }
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: (function () {
-            if (process.platform === 'darwin') return 'Alt+Command+I'
-            return 'Ctrl+Shift+I'
-          })(),
-          click (item, focusedWindow) {
-            if (focusedWindow) focusedWindow.toggleDevTools()
-          }
-        },
-        {
-          label: 'Show Audio Process',
-          accelerator: process.platform === 'darwin'
+viewMenu.splice(1, 0, {
+  label: 'Hard Reload (Clear Cache)',
+  accelerator: 'CmdOrCtrl+Shift+R',
+  click (item, focusedWindow) {
+    if (focusedWindow) focusedWindow.webContents.reloadIgnoringCache()
+  }
+})
+
+viewMenu.push({
+  label: 'Show Audio Process',
+  accelerator: process.platform === 'darwin'
             ? 'Alt+Command+P'
             : 'Ctrl+Shift+P',
-          click: () => audio.toggleDevTools()
-        }
-      ]
-    },
-    {
-      label: 'Window',
-      role: 'window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'CmdOrCtrl+M',
-          role: 'minimize'
-        },
-        {
-          label: 'Close',
-          accelerator: 'CmdOrCtrl+W',
-          role: 'close'
-        }
-      ]
-    },
-    {
-      label: 'Help',
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click () { shell.openExternal(pkg.homepage) }
-        },
-        {
-          label: 'Report an Issue',
-          click () { shell.openExternal(pkg.bugs.url) }
-        }
-      ]
-    }
-  ]
+  click: () => audio.toggleDevTools()
+})
 
-  if (process.platform === 'darwin') {
-    var name = 'HyperAmp'
+helpMenu[0] = {
+  label: 'Learn More',
+  click () { shell.openExternal(pkg.homepage) }
+}
 
-    template.unshift({
-      label: name,
-      submenu: [
-        {
-          label: 'About ' + name,
-          role: 'about'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Services',
-          role: 'services',
-          submenu: []
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Hide ' + name,
-          accelerator: 'Command+H',
-          role: 'hide'
-        },
-        {
-          label: 'Hide Others',
-          accelerator: 'Command+Alt+H',
-          role: 'hideothers'
-        },
-        {
-          label: 'Show All',
-          role: 'unhide'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Quit',
-          accelerator: 'Command+Q',
-          click () { app.quit() }
-        }
-      ]
-    })
+helpMenu[1] = {
+  label: 'Report an Issue',
+  click () { shell.openExternal(pkg.bugs.url) }
+}
 
-    template.filter(function (el) {
-      return el.label === 'Window'
-    })[0].submenu.push(
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Bring All to Front',
-        role: 'front'
-      }
-    )
-  }
-
-  return template
+function init () {
+  var menu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(menu)
 }
