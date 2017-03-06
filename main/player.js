d8278f210b3d4fe50d2bf3a2dfdd5dbca43d216f
Bret Comnes <bcomnes@gmail.com>
2017-03-05 19:29:35 -0800

    manage window state on player

--- main/player.js
+++ main/player.js
@@ -1,19 +1,27 @@
 var { BrowserWindow } = require('electron')
+var windowStateKeeper = require('electron-window-state')
 var path = require('path')
 var PLAYER_WINDOW = 'file://' + path.resolve(__dirname, '..', 'renderer', 'index.html')
 var player = module.exports = {
   init,
-  win: null
+  win: null,
+  windowState: null
 }
 
 require('electron-debug')({ showDevTools: true })
 require('electron-context-menu')()
 
 function init () {
+  player.windowState = windowStateKeeper({
+    width: 800,
+    height: 600
+  })
   var win = player.win = new BrowserWindow({
     title: 'Hyper Amp',
-    width: 800,
-    height: 600,
+    x: player.windowState.x,
+    y: player.windowState.y,
+    width: player.windowState.width,
+    height: player.windowState.height,
     minWidth: 275,
     minHeight: 40,
     titleBarStyle: 'hidden-inset',
@@ -21,6 +29,8 @@ function init () {
     show: false,
     thickFrame: true
   })
+
+  player.windowState.manage(win)
 
   win.loadURL(PLAYER_WINDOW)
 
