ff8fb5c0f06ac342851706113d6a8a633e356b22
Bret Comnes <bcomnes@gmail.com>
2017-03-14 15:09:09 -0700

    start porting choo5

--- renderer/index.js
+++ renderer/index.js
@@ -1,22 +1,17 @@
 var choo = require('choo')
-var log = require('choo-log')
+// var log = require('choo-log')
 var app = window.hyperamp = choo()
+var mainView = require('./pages/main')
+var preferences = require('./pages/preferences')
 var entypoSprite = require('entypo').getNode()
 document.body.insertBefore(entypoSprite, document.body.firstChild)
 
-app.use(log())
-
-window.files = []
+// app.use(log())
 
 app.model(require('./models/config'))
 app.model(require('./models/player'))
 app.model(require('./models/library'))
 
-app.router({ default: '/' }, [
-  ['/', require('./pages/main')],
-  ['/preferences', require('./pages/preferences')]
-])
-var tree = app.start()
-document.body.querySelector('#app').appendChild(tree)
-
-window.app = app
+app.route('/', mainView)
+app.route('/preferences', preferences)
+app.mount('#app')
