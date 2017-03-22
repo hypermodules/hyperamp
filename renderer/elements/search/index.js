01f574d0d11c6ff7794f3dd74ac2059f3adb2bee
Bret Comnes <bcomnes@gmail.com>
2017-03-21 20:40:55 -0700

    remove nanocomponent where its not used

--- renderer/elements/search/index.js
+++ renderer/elements/search/index.js
@@ -1,18 +1,16 @@
 var html = require('choo/html')
 var formStyles = require('../form/styles')
-var component = require('nanocomponent')
+// var component = require('nanocomponent')
 var styles = require('./styles')
 
-var search = component({
-  render: function ({ oninput, value }) {
-    return html`
+function search ({ oninput, value }) {
+  return html`
     <input type="search"
       class="${formStyles.formControl} ${styles.searchInput}"
       placeholder="Search"
       value="${value}"
       oninput=${oninput}>
   `
-  }
-})
+}
 
 module.exports = search
