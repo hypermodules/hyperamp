74163d95e53f1e079a5b8304e6ce62c87438b1c1
Nate Goldman <ungoldman@gmail.com>
2017-06-30 22:40:50 -0700

    use splash as default album art

--- renderer/elements/artwork/index.js
+++ renderer/elements/artwork/index.js
@@ -2,6 +2,8 @@ var html = require('choo/html')
 var Component = require('cache-component')
 var styles = require('./styles')
 var fileUrlFromPath = require('file-url')
+var path = require('path')
+var defaultBG = path.resolve(__dirname, '../../../splash.png')
 
 class Artwork extends Component {
   constructor (opts) {
@@ -13,7 +15,7 @@ class Artwork extends Component {
   }
 
   _render (artworkPath) {
-    var fileUrl = artworkPath ? fileUrlFromPath(artworkPath) : null
+    var fileUrl = artworkPath ? fileUrlFromPath(artworkPath) : defaultBG
     return html`
       <div class="${styles.albumCover}">
         <div class="${styles.albumArt}" style="background-image: ${fileUrl ? 'url(' + fileUrl + ')' : ''}"></div>
