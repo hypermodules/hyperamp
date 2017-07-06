6fe6a5d6f73a8a74406131ba03bda9a4b713f5aa
Bret Comnes <bcomnes@gmail.com>
2017-07-06 13:54:01 -0700

    windows fixes

--- renderer/elements/player/artwork.js
+++ renderer/elements/player/artwork.js
@@ -15,7 +15,7 @@ class Artwork extends Component {
   }
 
   _render (artworkPath) {
-    var fileUrl = artworkPath ? fileUrlFromPath(artworkPath) : defaultBG
+    var fileUrl = artworkPath ? fileUrlFromPath(artworkPath) : fileUrlFromPath(defaultBG)
     return html`
       <div class="${styles.albumCover}">
         <div class="${styles.albumArt}" style="background-image: ${fileUrl ? 'url(' + fileUrl + ')' : ''}"></div>
