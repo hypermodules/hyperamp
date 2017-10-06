018b66e96075c38d0028578280c5633509a745f2
Bret Comnes <bcomnes@gmail.com>
2017-10-06 11:47:41 -0700

    Revert "Fix icon"
    This reverts commit 5ac72a8ee208fd532ad5421bab7d9edb12bcfe38.

--- main/lib/audio-library/sort.js
+++ main/lib/audio-library/sort.js
@@ -4,28 +4,28 @@ function sort ([keyA, aObj], [keyB, bObj]) {
       // if (aObj.albumartist[0] > bObj.albumartist[0]) return 1
 
       // sort by artist
-  // if (aObj.artist[0] < bObj.artist[0]) return -1
-  // if (aObj.artist[0] > bObj.artist[0]) return 1
-//
-  //    // then by album
-  // if (aObj.album < bObj.album) return -1
-  // if (aObj.album > bObj.album) return 1
-//
-  //    // then by disc no
-  // if (aObj.disk.no < bObj.disk.no) return -1
-  // if (aObj.disk.no > bObj.disk.no) return 1
-//
-  //    // then by disc no
-  // if (aObj.track.no < bObj.track.no) return -1
-  // if (aObj.track.no > bObj.track.no) return 1
-//
-  //    // then by title
-  // if (aObj.title < bObj.title) return -1
-  // if (aObj.title > bObj.title) return 1
+  if (aObj.artist[0] < bObj.artist[0]) return -1
+  if (aObj.artist[0] > bObj.artist[0]) return 1
+
+      // then by album
+  if (aObj.album < bObj.album) return -1
+  if (aObj.album > bObj.album) return 1
+
+      // then by disc no
+  if (aObj.disk.no < bObj.disk.no) return -1
+  if (aObj.disk.no > bObj.disk.no) return 1
+
+      // then by disc no
+  if (aObj.track.no < bObj.track.no) return -1
+  if (aObj.track.no > bObj.track.no) return 1
+
+      // then by title
+  if (aObj.title < bObj.title) return -1
+  if (aObj.title > bObj.title) return 1
 
       // then by filepath
-  if (aObj.filepath.toLowerCase() < bObj.filepath.toLowerCase()) return -1
-  if (aObj.filepath.toLowerCase() > bObj.filepath.toLowerCase()) return 1
+  if (aObj.filepath < bObj.filepath) return -1
+  if (aObj.filepath > bObj.filepath) return 1
   return 0
 }
 
