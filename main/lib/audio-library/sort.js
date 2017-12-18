37f149b5f3ae900a3b9ca15b608af0b612873346
Nate Goldman <ungoldman@gmail.com>
2017-12-17 19:22:33 -0800

    fix(scanning): uncaught TypeError from missing "no" prop (#289)

--- main/lib/audio-library/sort.js
+++ main/lib/audio-library/sort.js
@@ -1,31 +1,44 @@
-function sort ([keyA, aObj], [keyB, bObj]) {
-      // sort by albumartist
-      // if (aObj.albumartist[0] < bObj.albumartist[0]) return -1
-      // if (aObj.albumartist[0] > bObj.albumartist[0]) return 1
+var existy = require('existy')
 
-      // sort by artist
+function sort ([keyA, aObj], [keyB, bObj]) {
+  // sort by albumartist
+  // if (aObj.albumartist[0] < bObj.albumartist[0]) return -1
+  // if (aObj.albumartist[0] > bObj.albumartist[0]) return 1
+
+  // sort by artist
   if (aObj.artist < bObj.artist) return -1
   if (aObj.artist > bObj.artist) return 1
 
-      // then by album
+  // then by album
   if (aObj.album < bObj.album) return -1
   if (aObj.album > bObj.album) return 1
 
-      // then by disc no
-  if (aObj.disk.no < bObj.disk.no) return -1
-  if (aObj.disk.no > bObj.disk.no) return 1
+  // then by disc no
+  var aHasDisk = existy(aObj.disk)
+  var bHasDisk = existy(bObj.disk)
 
-      // then by disc no
-  if (aObj.track.no < bObj.track.no) return -1
-  if (aObj.track.no > bObj.track.no) return 1
+  if (aHasDisk && bHasDisk) {
+    if (aObj.disk.no < bObj.disk.no) return -1
+    if (aObj.disk.no > bObj.disk.no) return 1
+  }
 
-      // then by title
+  // then by track no
+  var aHasTrack = existy(aObj.track)
+  var bHasTrack = existy(bObj.track)
+
+  if (aHasTrack && bHasTrack) {
+    if (aObj.track.no < bObj.track.no) return -1
+    if (aObj.track.no > bObj.track.no) return 1
+  }
+
+  // then by title
   if (aObj.title < bObj.title) return -1
   if (aObj.title > bObj.title) return 1
 
-      // then by filepath
+  // then by filepath
   if (aObj.filepath < bObj.filepath) return -1
   if (aObj.filepath > bObj.filepath) return 1
+
   return 0
 }
 
